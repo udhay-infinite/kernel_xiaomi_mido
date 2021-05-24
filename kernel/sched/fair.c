b82a91c2c0909072b105080f91551ad2a1b7977e
shashank1436 <9945shashank@gmail.com>
2021-05-24 21:21:33 +0530

    Revert "sched: Improve the scheduler"
    This reverts commit 55cbcecffd7671c22c5624f84cb44767515530fb.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12342,6 +12342,9 @@ static void walt_check_for_rotation(struct rq *src_rq)
 	if (!walt_rotation_enabled)
 		return;
 
+	if (got_boost_kick())
+		return;
+
 	if (is_max_capacity_cpu(src_cpu))
 		return;
 
